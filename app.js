8bf3a291c8c647839cff82dc36f5a6c800013628
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-04 17:13:25 -0500

    added initial javascript from lecture vid

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let cells = document.querySelectorAll(".row>div");
-console.log(cells);
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", cellClicked);
+}
+
+function cellClicked() {
+    event.target.textContent = "X";
+}
