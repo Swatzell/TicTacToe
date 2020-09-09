bf974975203c56d687f52ddea547a316b332e6da
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-08 22:08:16 -0500

    it's rough but i finally sorta finished this bitch

--- app.js
+++ app.js
@@ -46,7 +46,29 @@ function checkWinner() {
         console.log("WINNER!!!")
         gameOver = true;
 
+    } else if (cells[3].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[5].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[6].textContent === currentPlayer && cells[7].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[0].textContent === currentPlayer && cells[3].textContent === currentPlayer && cells[6].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[1].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[7].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[2].textContent === currentPlayer && cells[5].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[0].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+    } else if (cells[2].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[6].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
     }
+
 }
 
 function resetGame() {
