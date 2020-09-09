0c99f17ecb7e9997df76e608a42559757aa3ab2f
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-08 21:06:32 -0500

    finally got about 90 percent finished coding tic tac toe

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-let cells = document.querySelectorAll(".row>div");
+const cells = document.querySelectorAll(".row>div");
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+];
+
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", cellClicked);
 }
+let currentPlayer = "X"
+let gameOver = false;
 
 function cellClicked() {
-    event.target.textContent = "X";
+    if (gameOver) {
+        resetGame();
+        return;
+
+    }
+    if (event.target.textContent !== "") {
+        return;
+    };
+    event.target.textContent = currentPlayer;
+    checkWinner();
+    togglePlayer();
+
+}
+
+function togglePlayer() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O"
+    } else {
+        currentPlayer = "X"
+    }
+}
+
+function checkWinner() {
+    if (cells[0].textContent === currentPlayer && cells[1].textContent === currentPlayer && cells[2].textContent === currentPlayer) {
+        console.log("WINNER!!!")
+        gameOver = true;
+
+    }
+}
+
+function resetGame() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].textContent = "";
+    }
+    currentPlayer = "X";
+    gameOver = false;
 }
